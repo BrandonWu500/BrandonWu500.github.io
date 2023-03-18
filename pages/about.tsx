8a8a01752dd730d81028cab9342966f5c1017ad2
BrandonWu500 <brwu17@gmail.com>
2023-03-17 23:15:51 -0400

    update about me

--- pages/about.tsx
+++ pages/about.tsx
@@ -25,13 +25,13 @@ const about = () => {
           Tailwind CSS, and Responsive Web Design.
         </p>
         <p>
-          Currently, I am trying to gain more experience with the MERN Stack and
+          {`Currently, I am trying to gain more experience with the MERN Stack and
           MongoDB with Next.js (using the Mongoose library) by building on my
-          previous frontend projects.
+          previous frontend projects.`}
         </p>
         <p className="font-semibold">
-          I am also actively looking for a job as a front-end developer or React
-          developer.
+          I am also actively looking for a job as a Frontend Web, React, or UI
+          Developer.
         </p>
         <p>
           <span>Link to my Github: </span>
