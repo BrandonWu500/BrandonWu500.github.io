28999f7d07a96bd9ead07c6f8a5d36db8e86f95e
BrandonWu500 <brwu17@gmail.com>
2023-04-15 14:50:47 -0400

    add sort projects feature

---
+++ data/projectsIndex.ts
@@ -0,0 +1,69 @@
+export const projectsInfo = [
+  {
+    id: 1,
+    title: ["MERN Stack", "Shopping App"],
+    img: "/static/screenshots/mern-shop/landing-page.png",
+    link: "/projects/mernShop",
+  },
+  {
+    id: 2,
+    title: ["Admin Dashboard"],
+    img: "/static/screenshots/admin-firebase/landing-page.png",
+    link: "/projects/adminDashFront",
+    sections: [
+      {
+        title: "Frontend only",
+        link: "/projects/adminDashFront",
+      },
+      {
+        title: "Full Stack with Firebase",
+        link: "/projects/adminDashFirebase",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: ["Food Delivery App", "with Next.js"],
+    img: "/static/screenshots/food-delivery-fullstack/landing-page-screenshot.png",
+    link: "/projects/motoEcomFront",
+    sections: [
+      {
+        title: "Frontend only",
+        link: "/projects/motoEcomFront",
+      },
+      {
+        title: "Full Stack with MongoDB",
+        link: "/projects/foodDeliveryFullStack",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: ["Netflix Clone Frontend", "with Next.js"],
+    img: "/static/screenshots/netflix-clone-front/landing-page.png",
+    link: "/projects/netflixCloneFront",
+  },
+  {
+    id: 5,
+    title: ["Car Auction Frontend", "with Three.js"],
+    img: "/static/screenshots/car-auction/landing-page.png",
+    link: "/projects/carAuctionFront",
+  },
+  {
+    id: 6,
+    title: ["Motorcycle E-Commerce", "Mobile App", "with React Native"],
+    img: "/static/screenshots/moto-ecom-full/1.png",
+    link: "/projects/motoEcomFront",
+    mobile: true,
+    sections: [
+      {
+        title: "Frontend only",
+        link: "/projects/motoEcomFront",
+      },
+      {
+        title: "Full Stack (MERN)",
+        link: "/projects/motoEcomFull",
+      },
+    ],
+  },
+];
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,8 +1,46 @@
 import ProjectWrapper from "@/components/ProjectWrapper";
+import { projectsInfo } from "@/data/projectsIndex";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const enum Sorts {
+  NEWEST,
+  OLDEST,
+}
 
 const Projects = () => {
+  const [sortType, setSortType] = useLocalStorage("sort", Sorts.NEWEST);
+  const [projectsList, setProjectsList] = useState(projectsInfo);
+
+  const sortProjects = (sortIdx: Sorts) => {
+    let newProjectsList = [...projectsList];
+    switch (sortIdx) {
+      case Sorts.NEWEST:
+        newProjectsList.sort((a, b) => b.id - a.id);
+        break;
+
+      case Sorts.OLDEST:
+        newProjectsList.sort((a, b) => a.id - b.id);
+        break;
+
+      default:
+        break;
+    }
+    setProjectsList(newProjectsList);
+    setSortType(sortIdx);
+  };
+
+  useEffect(() => {
+    sortProjects(sortType);
+  }, []);
+
+  const handleSortChange = (e: any) => {
+    const sortIdx = Number(e.target.value);
+    sortProjects(sortIdx);
+  };
+
   return (
     <ProjectWrapper>
       <div className="mb-12">
@@ -10,159 +48,86 @@ const Projects = () => {
         <p className="text-center text-lg">
           Click on a project to learn more about it.
         </p>
+        <div className="mx-auto my-5 flex justify-center items-center gap-5">
+          <label className="text-center font-semibold" htmlFor="sort">
+            Sort Projects By:
+          </label>
+          <select
+            name="sort"
+            id="sort"
+            className="p-2 dark:bg-inherit"
+            value={sortType}
+            onChange={handleSortChange}
+          >
+            <option value={Sorts.NEWEST}>Newest</option>
+            <option value={Sorts.OLDEST}>Oldest</option>
+          </select>
+        </div>
       </div>
       <ul className="min-h-screen space-y-12 text-center">
-        <li>
-          <div className="font-semibold text-lg grid grid-cols-2 items-center gap-5">
-            <div>
-              <Link href="/projects/mernShop" className="font-bold">
-                <p>MERN Stack</p> <p>Shopping App</p>
-              </Link>
-            </div>
-            <Link href="/projects/mernShop">
-              <Image
-                src="/static/screenshots/mern-shop/landing-page.png"
-                alt=""
-                width={500}
-                height={500}
-                className="object-contain mb-5"
-              />
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div className="font-medium text-lg grid grid-cols-2 items-center gap-5">
-            <div className="-translate-y-2">
-              <Link href="/projects/adminDashFront" className="font-bold">
-                <p className="mb-4">Admin Dashboard</p>
-              </Link>
-              <ol className="list-decimal pl-5 space-y-4 mt-2 text-start">
-                <li>
-                  <Link href="/projects/adminDashFront" className="text-lg">
-                    Frontend only
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/projects/adminDashFirebase" className="text-lg">
-                    Fullstack with Firebase
-                  </Link>
-                </li>
-              </ol>
-            </div>
-            <Link href="/projects/adminDashFront">
-              <Image
-                src="/static/screenshots/admin-firebase/landing-page.png"
-                alt=""
-                width={500}
-                height={500}
-                className="object-contain mb-5"
-              />
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div className="font-medium text-lg grid grid-cols-2 items-center gap-5">
-            <div className="-translate-y-2">
-              <Link href="/projects/foodDeliveryFront" className="font-bold">
-                <p>Food Delivery App</p> <p>with Next.js</p>
-              </Link>
-              <ol className="list-decimal pl-5 space-y-4 mt-2 text-start">
-                <li>
-                  <Link href="/projects/foodDeliveryFront" className=" text-lg">
-                    Frontend only
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/projects/foodDeliveryFullStack"
-                    className=" text-lg"
-                  >
-                    Fullstack with MongoDB
-                  </Link>
-                </li>
-              </ol>
-            </div>
-            <Link href="/projects/foodDeliveryFront">
-              <Image
-                src="/static/screenshots/food-delivery-fullstack/landing-page-screenshot.png"
-                alt=""
-                width={500}
-                height={500}
-                className="object-contain mb-5"
-              />
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div className="font-semibold text-lg grid grid-cols-2 items-center gap-5">
-            <Link href="/projects/netflixCloneFront" className="font-bold">
-              <div>
-                <p>Netflix Clone Frontend</p> <p>with Next.js</p>
-              </div>
-            </Link>
-            <Link href="/projects/netflixCloneFront">
-              <Image
-                src="/static/screenshots/netflix-clone-front/landing-page.png"
-                alt=""
-                width={500}
-                height={500}
-                className="object-contain mb-5"
-              />
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div className="font-semibold text-lg grid grid-cols-2 items-center gap-5">
-            <Link href="/projects/carAuctionFront" className="font-bold">
-              <div>
-                <p>Car Auction Frontend</p> <p>with Three.js</p>
-              </div>
-            </Link>
-            <Link href="/projects/carAuctionFront">
-              <Image
-                src="/static/screenshots/car-auction/landing-page.png"
-                alt=""
-                width={500}
-                height={500}
-                className="object-contain mb-5"
-              />
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div className="font-medium text-lg grid grid-cols-2 items-center gap-5">
-            <div className="-translate-y-2">
-              <Link href="/projects/motoEcomFront" className="font-bold">
+        {projectsList.map((project) => (
+          <li key={project.id}>
+            {!project?.sections && (
+              <div className="font-semibold text-lg grid grid-cols-2 items-center gap-5">
                 <div>
-                  <p>Motorcycle E-Commerce</p>
-                  <p>Mobile App</p>
-                  <p>with React Native</p>
+                  <Link href={project.link} className="font-bold">
+                    {project.title.map((line, idx) => (
+                      <p key={idx}>{line}</p>
+                    ))}
+                  </Link>
                 </div>
-              </Link>
-              <ol className="list-decimal pl-5 space-y-4 mt-2 text-start">
-                <li>
-                  <Link href="/projects/motoEcomFront" className="text-lg">
-                    Frontend only
+                <Link href={project.link}>
+                  <Image
+                    src={project.img}
+                    alt=""
+                    width={500}
+                    height={500}
+                    className="object-contain mb-5"
+                  />
+                </Link>
+              </div>
+            )}
+            {project?.sections && (
+              <div className="font-medium text-lg grid grid-cols-2 items-center gap-5">
+                <div className="-translate-y-2">
+                  <Link href={project.link} className="font-bold">
+                    {project.title.map((line, idx) => (
+                      <p key={idx}>{line}</p>
+                    ))}
                   </Link>
-                </li>
-                <li>
-                  <Link href="/projects/motoEcomFull" className="text-lg">
-                    {`Full Stack (MERN)`}
-                  </Link>
-                </li>
-              </ol>
-            </div>
-            <Link href="/projects/motoEcomFront">
-              <Image
-                src="/static/screenshots/moto-ecom-full/1.png"
-                alt=""
-                width={200}
-                height={200}
-                className="object-contain mx-auto"
-              />
-            </Link>
-          </div>
-        </li>
+                  <ol className="list-decimal pl-5 space-y-4 mt-2 text-start">
+                    {project.sections.map((section, idx) => (
+                      <li key={idx}>
+                        <Link href={section.link} className="text-lg">
+                          {section.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+                <Link href={project.link}>
+                  {project?.mobile ? (
+                    <Image
+                      src={project.img}
+                      alt=""
+                      width={200}
+                      height={200}
+                      className="object-contain mx-auto"
+                    />
+                  ) : (
+                    <Image
+                      src={project.img}
+                      alt=""
+                      width={500}
+                      height={500}
+                      className="object-contain mb-5"
+                    />
+                  )}
+                </Link>
+              </div>
+            )}
+          </li>
+        ))}
       </ul>
     </ProjectWrapper>
   );
